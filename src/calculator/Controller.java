caf89fb824e51c52db0a82a44569c23cef466da6
lahl <domee.lahl@gmail.com>
2020-11-10 16:22:50 +0100

    Add logic skeleton to controller

--- src/calculator/Controller.java
+++ src/calculator/Controller.java
@@ -1,4 +1,45 @@
 package calculator;
 
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.TextField;
+
 public class Controller {
+
+    @FXML public TextField fieldValue1;
+    @FXML public TextField fieldValue2;
+    @FXML public TextField fieldResult;
+    @FXML public Button btnAddition;
+    @FXML public Button btnSubtraction;
+    @FXML public Button btnMultiplication;
+    @FXML public Button btnDivision;
+    @FXML public Button btnPower;
+    @FXML public Button btnModulo;
+    @FXML public Button btnModuloInverse;
+
+    public void btnAdditionOnAction(ActionEvent actionEvent) {
+    }
+
+
+    public void btnSubtractionOnAction(ActionEvent actionEvent) {
+    }
+
+    public void btnMultiplicationOnAction(ActionEvent actionEvent) {
+    }
+
+    public void btnDivisionOnAction(ActionEvent actionEvent) {
+    }
+
+    public void btnPowerOnAction(ActionEvent actionEvent) {
+    }
+
+    public void btnModuloOnAction(ActionEvent actionEvent) {
+    }
+
+    public void btnModuloInverseOnAction(ActionEvent actionEvent) {
+    }
+
+    public void btnResetOnAction(ActionEvent actionEvent) {
+    }
 }
