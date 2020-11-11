1910ab06dd3f9b691822ec3c1a4c5dd3f9c4318e
Tesselay Avari <domee.lahl@gmail.com>
2020-11-11 13:17:25 +0100

    Add deactivation of other buttons on button click

--- src/calculator/Controller.java
+++ src/calculator/Controller.java
@@ -2,8 +2,12 @@ package calculator;
 
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.scene.Node;
 import javafx.scene.control.Button;
 import javafx.scene.control.TextField;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class Controller {
 
@@ -22,13 +26,40 @@ public class Controller {
     private double value1;
     private double value2;
 
+    private List<Button> buttons = new ArrayList<>();
+
+    private void addButtons() {
+        buttons.add(btnAddition);
+        buttons.add(btnSubtraction);
+        buttons.add(btnMultiplication);
+        buttons.add(btnDivision);
+        buttons.add(btnPower);
+        buttons.add(btnModulo);
+        buttons.add(btnFloorDivision);
+    }
+
     private void updateValues() {
         // For more efficiency, the values are only updated once one of the operand buttons is pressed.
-        try {
-            value1 = Double.parseDouble(fieldValue1.getText());
-            value2 = Double.parseDouble(fieldValue2.getText());
-        } catch (NumberFormatException e) {
-            resetFields();
+        value1 = Double.parseDouble(fieldValue1.getText());
+        value2 = Double.parseDouble(fieldValue2.getText());
+    }
+
+    private void deactivateButtons(String id) {
+        if (buttons.size() == 0) {
+            addButtons();
+        }
+
+        for (Button button : buttons) {
+            button.setDisable(true);
+            if (button.getId().equals(id)) {
+                button.setDisable(false);
+            }
+        }
+    }
+
+    private void activateButtons() {
+        for (Button button : buttons) {
+            button.setDisable(false);
         }
     }
 
@@ -39,6 +70,7 @@ public class Controller {
     }
 
     public void btnAdditionOnAction(ActionEvent actionEvent) {
+        deactivateButtons(((Node) actionEvent.getSource()).getId());
         updateValues();
         double result = value1 + value2;
         fieldResult.setText(String.valueOf(result));
@@ -46,24 +78,28 @@ public class Controller {
 
 
     public void btnSubtractionOnAction(ActionEvent actionEvent) {
+        deactivateButtons(((Node) actionEvent.getSource()).getId());
         updateValues();
         double result = value1 - value2;
         fieldResult.setText(String.valueOf(result));
     }
 
     public void btnMultiplicationOnAction(ActionEvent actionEvent) {
+        deactivateButtons(((Node) actionEvent.getSource()).getId());
         updateValues();
         double result = value1 * value2;
         fieldResult.setText(String.valueOf(result));
     }
 
     public void btnDivisionOnAction(ActionEvent actionEvent) {
+        deactivateButtons(((Node) actionEvent.getSource()).getId());
         updateValues();
         double result = value1 / value2;
         fieldResult.setText(String.valueOf(result));
     }
 
     public void btnPowerOnAction(ActionEvent actionEvent) {
+        deactivateButtons(((Node) actionEvent.getSource()).getId());
         updateValues();
 
         double result = value1;
@@ -74,12 +110,14 @@ public class Controller {
     }
 
     public void btnModuloOnAction(ActionEvent actionEvent) {
+        deactivateButtons(((Node) actionEvent.getSource()).getId());
         updateValues();
         double result = value1 % value2;
         fieldResult.setText(String.valueOf(result));
     }
 
     public void btnFloorDivisionOnAction(ActionEvent actionEvent) {
+        deactivateButtons(((Node) actionEvent.getSource()).getId());
         updateValues();
         // A simple typecast suffices for flooring, since the fractional digits will just be cut off.
         int result = (int) (value1 / value2);
@@ -88,5 +126,6 @@ public class Controller {
 
     public void btnResetOnAction(ActionEvent actionEvent) {
         resetFields();
+        activateButtons();
     }
 }
