99e50fe8e7d01c831369511cbba0269c0e3af8cd
Tesselay Avari <domee.lahl@gmail.com>
2020-11-11 10:31:28 +0100

    Add logic to actions

--- src/calculator/Controller.java
+++ src/calculator/Controller.java
@@ -22,34 +22,61 @@ public class Controller {
     private int value2;
 
     private void updateValues() {
+        // For more efficiency, the values are only updated once one of the operand buttons is pressed.
         value1 = Integer.parseInt(fieldValue1.getText());
         value2 = Integer.parseInt(fieldValue2.getText());
     }
 
     public void btnAdditionOnAction(ActionEvent actionEvent) {
-
-
+        updateValues();
+        int result = value1 + value2;
+        fieldResult.setText(String.valueOf(result));
     }
 
 
     public void btnSubtractionOnAction(ActionEvent actionEvent) {
+        updateValues();
+        int result = value1 - value2;
+        fieldResult.setText(String.valueOf(result));
     }
 
     public void btnMultiplicationOnAction(ActionEvent actionEvent) {
+        updateValues();
+        int result = value1 * value2;
+        fieldResult.setText(String.valueOf(result));
     }
 
     public void btnDivisionOnAction(ActionEvent actionEvent) {
+        updateValues();
+        int result = value1 / value2;
+        fieldResult.setText(String.valueOf(result));
     }
 
     public void btnPowerOnAction(ActionEvent actionEvent) {
+        updateValues();
+
+        int result = value1;
+        for (int i = 0; i <= value2; i++) {
+            result *= value1;
+        }
+        fieldResult.setText(String.valueOf(result));
     }
 
     public void btnModuloOnAction(ActionEvent actionEvent) {
+        updateValues();
+        int result = value1 % value2;
+        fieldResult.setText(String.valueOf(result));
     }
 
     public void btnModuloInverseOnAction(ActionEvent actionEvent) {
+        updateValues();
+        int result = value1 + value2;
+        fieldResult.setText(String.valueOf(result));
     }
 
     public void btnResetOnAction(ActionEvent actionEvent) {
+        fieldValue1.setText("");
+        fieldValue2.setText("");
+        fieldResult.setText("");
     }
 }
